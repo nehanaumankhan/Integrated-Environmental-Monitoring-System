a0d9bd6d71a20876750df5a4d90b9d5f7593f16a
Manahil4 <142149713+Manahil4@users.noreply.github.com>
2024-01-10 08:34:11 -0800

    Create weather.c

---
+++ weather.c
@@ -0,0 +1,17 @@
+// weather.c
+#include "weather.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+size_t write_callback(void *contents, size_t size, size_t nmemb, char **output) {
+
+}
+
+void processWeatherData(const char *response, const char *processedDataFile) {
+
+}
+
+int fetchDataAndProcess(void) {
+
+}
