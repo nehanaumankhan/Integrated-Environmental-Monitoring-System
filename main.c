63cd60bc0d9398444a87d1e0a764c06671b19d9c
Manahil4 <142149713+Manahil4@users.noreply.github.com>
2024-01-13 04:06:51 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
-// main.c
 #include <stdio.h>
-#include "weather.h"
+#include <curl/curl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cJSON/cJSON.h"
+#include "fetchData.h"
+#include "processData.h"
+#include "anomaly.h"
+
+#define ReponseFile "response.txt"
+#define ProcessedDataFile "process.csv"
+#define AverageDataFile  "average.csv"
+
 
 int main(void) {
-    return fetchDataAndProcess();
+    time_t t = time(NULL);
+    struct tm *tm_info = localtime(&t);
+    char dateBuffer[11];  // Buffer for date (YYYY-MM-DD)
+    char timeBuffer[9];   // Buffer for time (HH:MM:SS)
+    strftime(dateBuffer, sizeof(dateBuffer), "%Y-%m-%d", tm_info);
+    strftime(timeBuffer, sizeof(timeBuffer), "%H:%M:%S", tm_info);
+
+    float avgHumidity, avgPressure,humidity,pressure;
+
+    char *response = perform_http_request(ReponseFile);
+   
+    processWeatherData(response, ProcessedDataFile,dateBuffer,timeBuffer,&humidity,&pressure);
+
+    calculate_average(ProcessedDataFile,AverageDataFile,&avgHumidity,&avgPressure);
+
+    detect_anomaly(avgPressure,avgHumidity,pressure,humidity,dateBuffer,timeBuffer);
+    
+    return 0;
 }
