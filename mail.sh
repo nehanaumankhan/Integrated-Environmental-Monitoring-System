52df81fff64ecced4b39780f1afb5e2fbf014b8f
nehanaumankhan <132737279+nehanaumankhan@users.noreply.github.com>
2024-02-02 05:08:34 -0800

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -9,9 +9,9 @@ arg1="$1"
 arg2="$2"
 arg3="$3"
 
-TO="ayeshaahmed45678@gmail.com"
+TO="<Email>@gmail.com"
 SUBJECT="***Anomaly in weather***"
 BODY="An anomaly is detected in the weather data at:\n\nDate: $arg2\nTime: $arg3\n\n        ***ANOMALY***        \n\n$arg1\n\nPlease find the attached report of weather data"
 ATTACHMENT_FILE="/home/ayesha/Desktop/Ayesha/CEW/process.csv"
 
-sendemail -f "ayeshaahmed45678@gmail.com" -t "$TO" -u "$SUBJECT" -m "$BODY" -a "$ATTACHMENT_FILE" -s "smtp.gmail.com:587" -o tls=yes -xu "ayeshaahmed45678@gmail.com" -xp "<Password>"
+sendemail -f "<Email>@gmail.com" -t "$TO" -u "$SUBJECT" -m "$BODY" -a "$ATTACHMENT_FILE" -s "smtp.gmail.com:587" -o tls=yes -xu "<Email>@gmail.com" -xp "<Password>"
